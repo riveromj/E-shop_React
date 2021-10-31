39fe595c378d9bef9be074962b4a32b35e09e331
Marigen Rivero <marigen@gmail.com>
2021-10-31 20:39:27 +0100

    add total to shopping cart

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -3,8 +3,19 @@ import AppContext from '../context/AppContext'
 import OrderItem from '../components/OrderItem';
 import '../styles/MyOrder.scss';
 import flechita from'@icons/flechita.svg';
+
+
 const MyOrder = () => {
-  const { state } = useContext(AppContext);
+
+ const { state } = useContext(AppContext);
+
+	const sumTotal = () => {
+		const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+    console.log(sum,'+++++')
+		return sum;
+	}
+
   return (
     <aside className="MyOrder">
       <div className="title-container">
@@ -18,7 +29,7 @@ const MyOrder = () => {
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>$ {sumTotal()}</p>
         </div>
         <button className="primary-button">
           Checkout
