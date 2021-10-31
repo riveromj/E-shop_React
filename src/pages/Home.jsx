6da2916b8435f87210b3d1cbed04bd0320ebc390
Marigen Rivero <marigen@gmail.com>
2021-10-31 17:04:52 +0100

    view login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ProductList from '../containers/ProductList';
-import  Header  from '../components/Header';
+
 const Home = () => {
     return (
         <>
-            <Header/>
             <ProductList />
         </>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/Login.scss'
+import logo from '@logos/logo_yard_sale.svg'
 
 const Login = () => {
+  const form = useRef(null);
+
+  const handleSubmit = (event) => {
+    //traemos todo los valores cuando enviamo el formulario
+    event.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password')
+    }
+    console.log(data,'-----')
+  }
   return (
     <div className="login">
-      <div className="form-container">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-
-        <h1 className="title">Create a new password</h1>
-        <p className="subtitle">Enter a new password for you account</p>
-
-        <form action="/" className="form">
-          <label for="password" className="label">Password</label>
-          <input type="password" id="password" placeholder="*********" className="input input-password" />
-
-          <label for="new-password" className="label">Password</label>
-          <input type="password" id="new-password" placeholder="*********" className="input input-password" />
-
-          <input type="submit" value="Confirm" className="primary-button login-button" />
+      <div className="login-container">
+        <img src={logo} alt="logo" className="logo" />
+        <form action="/" className="form" ref={form}>
+          <label htmlFor="email" className="label">Email address</label>
+          <input type="text" name="email" placeholder="user@example.com" className="input input-email" />
+          <label htmlFor="password" className="label">Password</label>
+          <input type="password" name="password" placeholder="*********" className="input input-password" />
+          <button
+            onClick={handleSubmit}
+            className="primary-button login-button">
+            Log in
+          </button>
+          <a href="/">Forgot my password</a>
         </form>
+        <button
+          className="secondary-button signup-button"
+        >
+          Sign up
+        </button>
       </div>
-    </div>
+    </div >
   );
 };
 
